7e4d9016087896d923d762da045dc4f29792eb40
anabiamaia <anabmaias0@gmail.com>
2022-03-21 14:01:46 -0300

    commit initial

---
+++ src/test/java/com/algaworks/crm/controller/ClienteControllerTest.java
@@ -0,0 +1,14 @@
+package com.algaworks.crm.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ClienteControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
